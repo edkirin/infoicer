1f2d9a94677693a85e692cb292010553afb77cc5
Eden <edkirin@gmail.com>
2020-08-17 15:11:23 +0200

    wheel setup

---
+++ infoicer/__init__.py
@@ -0,0 +1,1 @@
+from .infoicer import Invoice, InvoiceItem
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="infoicer", # Replace with your own username
+    version="0.0.1",
+    author="Eden Kirin",
+    author_email="edkirin@gmail.com",
+    description="Infoicer is a simple invoice items handler.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edkirin/infoicer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
